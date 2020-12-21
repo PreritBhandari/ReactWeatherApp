525907f8e3d75734f1e0db9ba5415a6211e789d2
Prerit Bhandari <patrioticprerit@gmail.com>
2020-12-21 22:20:23 +0545

    [Updated] with api

--- weatherappreact/src/App.js
+++ weatherappreact/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [details, setDet] = useState([]);
+  const [place, setPlace] = useState("");
+
+  const searchWeather = () => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=7e7353c9d8ee0cef59b7081e4c1cfd77`
+      )
+      .then((res) => {
+        console.log(res.data);
+        setDet(res.data);
+      });
+  };
+
+  const typedData = (event) => {
+    setPlace(event.target.value);
+  };
+
+  const formSubmit = (event) => {
+    event.preventDefault();
+    searchWeather();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Weather App</h1>
+      <br />
+      <br />
+      <form onSubmit={formSubmit}>
+        <input
+          value={place}
+          type="text"
+          placeholder="Enter city name"
+          onChange={typedData}
+        />
+
+        <input type="submit" value="Search" />
+      </form>
+      <br />
+      <br />
+      <h5>
+        City Name:{" "}
+        <text
+          style={{ fontStyle: "italic", fontFamily: "cursive", fontSize: 20 }}
         >
-          Learn React
-        </a>
-      </header>
+          {details.name}
+        </text>
+      </h5>
     </div>
   );
 }
