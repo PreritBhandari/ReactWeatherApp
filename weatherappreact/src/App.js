ff31704b478356ffa1105440e0bb21934c94da10
Prerit Bhandari <patrioticprerit@gmail.com>
2020-12-21 23:20:31 +0545

    [Updated] weatherapp

--- weatherappreact/src/App.js
+++ weatherappreact/src/App.js
@@ -4,7 +4,17 @@ import axios from "axios";
 
 function App() {
   const [details, setDet] = useState([]);
+  const [longitude, setLong] = useState("");
+  const [latitude, setLat] = useState("");
+  const [feels_like, setFeel] = useState("");
+  const [humidity, setHum] = useState("");
+  const [pressure, setPre] = useState("");
+  const [temperature, setTemp] = useState("");
+  const [temp_max, setTempMax] = useState("");
+  const [temp_min, setTempMin] = useState("");
+
   const [place, setPlace] = useState("");
+  const [gotdata, setGotData] = useState(false);
 
   const searchWeather = () => {
     axios
@@ -14,6 +24,15 @@ function App() {
       .then((res) => {
         console.log(res.data);
         setDet(res.data);
+        setLong(res.data.coord.lon);
+        setLat(res.data.coord.lat);
+        setFeel(res.data.main.feels_like);
+        setHum(res.data.main.humidity);
+        setPre(res.data.main.pressure);
+        setTemp(Math.round(res.data.main.temp - 273));
+        setTempMax(Math.round(res.data.main.temp_max - 273));
+        setTempMin(Math.round(res.data.main.temp_min - 273));
+        setGotData(true);
       });
   };
 
@@ -43,14 +62,107 @@ function App() {
       </form>
       <br />
       <br />
-      <h5>
-        City Name:{" "}
-        <text
-          style={{ fontStyle: "italic", fontFamily: "cursive", fontSize: 20 }}
-        >
-          {details.name}
-        </text>
-      </h5>
+      {gotdata ? (
+        <container>
+          <h5>
+            City Name:{" "}
+            <text
+              style={{
+                fontStyle: "italic",
+                fontFamily: "cursive",
+                fontSize: 20,
+              }}
+            >
+              {details.name}
+            </text>
+            <br />
+            <br />
+            Latitude:{" "}
+            <text style={{ fontStyle: "italic", fontSize: 20 }}>
+              {longitude}
+            </text>
+            <br />
+            <br />
+            Longitude:{" "}
+            <text style={{ fontStyle: "italic", fontSize: 20 }}>
+              {latitude}
+            </text>
+            <br />
+            <br />
+            Feels Like:{" "}
+            <text
+              style={{
+                fontStyle: "italic",
+                fontFamily: "cursive",
+                fontSize: 20,
+              }}
+            >
+              {feels_like} &deg;C
+            </text>
+            <br />
+            <br />
+            Pressure:{" "}
+            <text
+              style={{
+                fontStyle: "italic",
+                fontFamily: "cursive",
+                fontSize: 20,
+              }}
+            >
+              {pressure}
+            </text>
+            <br />
+            <br />
+            Humidity:{" "}
+            <text
+              style={{
+                fontStyle: "italic",
+                fontFamily: "cursive",
+                fontSize: 20,
+              }}
+            >
+              {humidity}
+            </text>
+            <br />
+            <br />
+            Temperature:{" "}
+            <text
+              style={{
+                fontStyle: "italic",
+                fontFamily: "cursive",
+                fontSize: 20,
+              }}
+            >
+              {temperature} &deg;C
+            </text>
+            <br />
+            <br />
+            Max Temperature:{" "}
+            <text
+              style={{
+                fontStyle: "italic",
+                fontFamily: "cursive",
+                fontSize: 20,
+              }}
+            >
+              {temp_max} &deg;C
+            </text>
+            <br />
+            <br />
+            Min Temperature:{" "}
+            <text
+              style={{
+                fontStyle: "italic",
+                fontFamily: "cursive",
+                fontSize: 20,
+              }}
+            >
+              {temp_min} &deg;C
+            </text>
+            <br />
+          </h5>
+        </container>
+      ) : null}
     </div>
   );
 }
